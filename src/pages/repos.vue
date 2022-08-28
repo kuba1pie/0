c946ddcabd64b213c47ebd5824f61243e2c39264
kuba1pie <kuba1pie@gmail.com>
2022-08-29 01:24:22 +0200

    feat: fetch instead octokit

--- src/pages/repos.vue
+++ src/pages/repos.vue
@@ -1,31 +1,21 @@
 <script setup lang="ts">
-// @ts-expect-error due to octokit types
-import { Octokit } from 'https://cdn.skypack.dev/@octokit/rest'
-import type { RootObject } from '~/types'
+import type { RootObject } from '../types'
 
 const store = reactive({
   results: [] as RootObject[], loading: false,
 })
-
-const octokit = new Octokit({
-  auth: import.meta.env.OCTOKIT,
-})
 async function ddd() {
-  const result = await octokit.request('GET /users/{username}/repos', {
-    username: 'kuba1pie',
-  })
-  store.results = result.data
+  fetch('https://api.github.com/users/kuba1pie/repos')
+    .then(response => response.json())
+    .then(data => store.results = (data))
 }
 try {
   store.loading = true
-
   ddd()
 }
 
 catch (error) {
   console.error(error)
-  // expected output: ReferenceError: nonExistentFunction is not defined
-  // Note - error messages will vary depending on browser
 }
 finally {
   store.loading = false
--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '~/': `${path.resolve(__dirname, 'src')}/`,
+      'node-fetch': 'isomorphic-fetch',
     },
   },
 
