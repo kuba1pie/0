c2911ed00630c4843594ec8981b9f59cf2bbcbd3
kuba1pie <kuba1pie@gmail.com>
2022-08-31 01:39:04 +0200

    feat: topics, styling

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="header">
-    <a
-      class="icon-btn mx-2 text-20 flex items-center m-5 justify-center " rel="noreferrer" href="https://github.com/kuba1pie"
-      target="_blank" title="GitHub"
-    >
+    <a class="icon-btn mx-2 text-20 flex items-center m-5 justify-center " rel="noreferrer"
+      href="https://github.com/kuba1pie" target="_blank" title="GitHub">
       <div i-carbon-logo-github />
       <div class="flex flex-col m-5">
         <span class="text-6">Kuba Pietruszewski</span>
