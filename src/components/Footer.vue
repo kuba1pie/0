8c1bf24fee18f40299bdc20adb1f2baf7c449211
kuba1pie <kuba1pie@gmail.com>
2022-08-31 01:46:50 +0200

    feat: test husky

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -9,17 +9,7 @@ const { t } = useI18n()
         <div i-carbon-campsite />
       </RouterLink>
 
-      <button class="icon-btn mx-2 !outline-none" :title="t('button.toggle_dark')" @click="toggleDark()">
-        <div i="carbon-sun dark:carbon-moon" />
-      </button>
-
-      <RouterLink class="icon-btn mx-2" to="/about" :title="t('button.about')">
-        <div i-carbon-dicom-overlay />
-      </RouterLink>
-
-      <a class="icon-btn mx-2" rel="noreferrer" href="https://github.com/kuba1pie" target="_blank" title="GitHub">
-        <div i-carbon-logo-github />
-      </a>
+      <button class="icon-btn mx-2 !outline-none" :title="t('button.toggle_dark')" @click="toggleDark()">        <div i="carbon-sun dark:carbon-moon" />      </button>      <RouterLink class="icon-btn mx-2" to="/about" :title="t('button.about')">        <div i-carbon-dicom-overlay />      </RouterLink>      <a class="icon-btn mx-2" rel="noreferrer" href="https://github.com/kuba1pie" target="_blank" title="GitHub">        <div i-carbon-logo-github />      </a>
 
       <a class="icon-btn mx-2" mailto="kontakt@kubapietruszewski.pl">
         <div i-mdi:mail />
